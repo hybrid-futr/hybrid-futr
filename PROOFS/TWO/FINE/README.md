7f565142129dc6d09bfb96d8127ccab2d6b16619
J.F. Ramirez <admin@hybridfutr.com>
2022-09-22 21:17:38 -0700

    9.22.2022 - 9:15P

--- PROOFS/TWO/FINE/README.md
+++ PROOFS/TWO/FINE/README.md
@@ -1,7 +1,7 @@
 # DASHBOARDS
 
-**FINE**
+**GENERAL**
 Each example ex#.py follows these guidelines:
-1. Remote Server
-2. Data [CSV]
-3. Tableau
+1. Local Server
+2. Py + SQL (Data)
+3. Py viz module
--- PROOFS/TWO/GENERAL/ex1.py
+++
@@ -1,3 +0,0 @@
-#Here I'm creating an Amazon VPC for use with a DB instance.
-#I'm using the following tutorial:
-#https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_Tutorials.WebServerDB.CreateVPC.html
