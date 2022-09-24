1dba55f413d246d388a3e2050e5d2bc9a66c0936
J.F. Ramirez <admin@hybridfutr.com>
2022-09-24 15:18:35 -0700

    Update webscraper.py

--- PROOFS/ONE/GENERAL/WEBSCRAPER/webscraper.py
+++ PROOFS/ONE/GENERAL/WEBSCRAPER/webscraper.py
@@ -2,14 +2,45 @@ import sqlite3
 import requests
 from bs4 import BeautifulSoup
 
+#METHODS
+
 #Request URL
-response = requests.get("http://books.toscrape.com")
-soup = BeautifulSoup(response.text, "html.parser")
-books = soup.find_all("article")
-print(books)
+def scrape_books(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    books = soup.find_all("article")
+    all_books = []
+    for book in books:
+            book_data = (get_title(book), get_price(book), get_rating(book))
+            all_books.append(book_data)
+    save_books(all_books)
+ 
+def save_books(all_books):
+    connection = sqlite3.connect("books.db")
+    c = connection.cursor() 
+    c.execute('''CREATE TABLE books
+        (title TEXT, price REAL, rating INTEGER)''')
+    c.executemany("INSERT INTO books values (?,?,?)", all_books)
+    connection.commit()
+    connection.close()
+    
+def get_title(book):
+    return book.find("h3").find("a")["title"]
 
-#Initialize bs4
+def get_price(book):
+    price = book.select(".price_color")[0].get_text()
+    return float(price.replace("£","").replace("Â",""))
 
-#Extract data
+def get_rating(book):
+    ratings = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+    paragraph = book.select(".star-rating")[0]
+    numero = paragraph.get_attribute_list("class")[-1]
+    return ratings[numero]
 
-#Save data to db
+scrape_books("http://books.toscrape.com")
+
+#INITIALIZE Beautiful Soup
+
+#EXTRACT DATA
+
+#SAVE DATA TO DB
